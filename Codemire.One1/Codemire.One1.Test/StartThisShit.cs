cf16fe01cb30bf99d38ca32724857301de974b38
Neil.McCoy <Neil.McCoy@FDAUSYDL6-NMC>
2017-01-07 21:31:19 +1100

    Project initialisation

---
+++ Codemire.One1/Codemire.One1.Test/StartThisShit.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Codemire.One1.Test
+{
+    [TestFixture]
+    public class StartThisShit
+    {
+        private One1EntryPoint _root;
+
+        [SetUp]
+        public void SetUp()
+        {
+            IMeetingRepository mockRepository = new MockRepository();
+            _root = new One1EntryPoint(mockRepository);
+        }
+
+        [Test]
+        public void CanInstantiateOneOnOne(){}
+
+        [Test]
+        public void CanCreateANewOneOnOneMeetingRecord()
+        {
+            _root.RecordMeeting(Guid.NewGuid(), "Lorenzo", "Silly pants");
+        }
+
+        [Test]
+        public void CanCreateAnActionForReportWithDueDate()
+        {
+            _root.NewActionForReport("Lorenzo", "Wear new shirt tomorrow", DateTime.Now.AddDays(1));
+        }
+
+        [Test]
+        public void CanCreateAnActionForReportWithNoDueDate()
+        {
+            _root.NewActionForReport("Natasha", "get to the shops and buy potatoes");
+        }
+
+        [Test]
+        public void CanCreateAnActionForDelegateWithDueDate()
+        {
+            _root.NewActionForDelegate("Robyn", "Wear new shirt tomorrow", DateTime.Now.AddDays(1));
+        }
+
+        [Test]
+        public void CanCreateAnActionForDelegateWithNoDueDate()
+        {
+            _root.NewActionForDelegate("James", "help me");
+        }
+
+        [Test]
+        public void CanCreateDiscussionPointForNextMeeting()
+        {
+            _root.AddDiscussionPoint("Anthony", "Why you no wiggle?");
+        }
+
+        [Test]
+        public void CanGetListOfAllDiscussionPoints()
+        {
+
+            _root.GetDiscussionPoints().Length.Should().Be(3);
+        }
+
+
+        public class MockRepository : IMeetingRepository
+        {
+            private string[] _discussionPoints;
+
+            public void AddDiscussionPoints(string[] discussionPoints)
+            {
+                _discussionPoints = discussionPoints;
+            }
+        }
+    }
+
+
+}
---
+++ Codemire.One1/Codemire.One1/One1EntryPoint.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Codemire.One1
+{
+    public class One1EntryPoint
+    {
+        private readonly IMeetingRepository _repository;
+
+        public One1EntryPoint(IMeetingRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void RecordMeeting(Guid guid, string report, string meetingNotes)
+        {
+            
+        }
+
+        public void NewActionForReport(string report, string actionText, DateTime dueDate)
+        {
+            
+        }
+
+        public void NewActionForReport(string report, string actionText)
+        {
+            
+        }
+
+        public void NewActionForDelegate(string delegateTo, string actionText, DateTime dueDate)
+        {
+            
+        }
+        
+        public void NewActionForDelegate(string delegateTo, string actionText)
+        {
+            
+        }
+
+        public void AddDiscussionPoint(string report, string discussionPoint)
+        {
+            
+        }
+
+        public string[] GetDiscussionPoints()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IMeetingRepository
+    {
+    }
+}
